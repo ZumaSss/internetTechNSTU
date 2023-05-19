d4a0abc8f4fe40d7cc7dc3a3a6a2e9d323b1d412
ivana <ivan.akkerman@list.ru>
2023-05-19 13:57:38 +0700

    release

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import userRouter from './routes/user.routes.js'
 
@@ -7,11 +12,9 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"))
-app.get('/', function (req, res) {
-    res.sendFile('index.html')
-})
+
 app.get('/menu', function (req, res) {
-    res.sendFile('C:\\Users\\ivana\\WebstormProjects\\internetTechNSTU\\public\\menu.html')
+    res.sendFile(__dirname + '/public/menu.html')
 })
 
 app.use('/', userRouter)
